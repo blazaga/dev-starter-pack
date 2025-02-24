2f8ebba2a61579000f8e1c3bbeb66fc84dcbbf98
Bon <bon@dnamicro.com>
2025-02-24 23:48:50 +0800

    correction

--- debian/sources/install_prerequisites.sh
+++ debian/sources/install_prerequisites.sh
@@ -6,7 +6,7 @@ install_prerequisites ()
 
   echo "Installing Default Packages";
   sudo apt update;
-  sudo apt install -y git curl build-essential htop net-toolst;
+  sudo apt install -y git curl build-essential htop net-tools;
   echo "Installing Default Packages - Done";
 
 }
