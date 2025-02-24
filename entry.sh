557828956d6fe0b6c2c088edc2a646ba2b4c8520
Bon <bon@dnamicro.com>
2025-02-24 23:31:09 +0800

    corrections

--- entry.sh
+++ entry.sh
@@ -7,8 +7,8 @@ source ./detect-os.sh;
 init(){
   local OS=$(detect_os|awk '{print tolower($0)}');
   case "$OS" in
-    *debian*|*ubuntu*) echo 1
-      source ./debian/entry.sh;
+    *debian*|*ubuntu*)
+      source ${pwd}/debian/entry.sh;
       entry;
       ;;
     *) echo "Cannot Install - Unsupported OS"
