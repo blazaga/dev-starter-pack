a38f5d7b88ca01df47bfb8f15a08243f6db010ce
Bon <bon@dnamicro.com>
2025-02-24 23:55:45 +0800

    add mosh

--- debian/sources/install_prerequisites.sh
+++ debian/sources/install_prerequisites.sh
@@ -6,7 +6,7 @@ install_prerequisites ()
 
   echo "Installing Default Packages";
   sudo apt update;
-  sudo apt install -y git curl build-essential htop net-tools;
+  sudo apt install -y git curl build-essential mosh htop net-tools;
   echo "Installing Default Packages - Done";
 
 }
