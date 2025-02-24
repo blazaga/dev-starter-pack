25fd7591f795854e29dce1775dca6fa8ba01cc35
Bon <bon@dnamicro.com>
2025-02-24 21:34:56 +0800

    add new file

---
+++ detect-os.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+function detect_distro() {
+  source /etc/releases;
+  local distro=ID;
+
+  echo "Detected Distro $distro"
+  case "$distro" in 
+    *ubuntu*|*debian*)
+      distro="debian";
+      ;;
+  esac
+  
+  echo $distro;
+
+}
+
+function detect_os() {
+  local os=$OSTYPE;
+
+  case "$os" in
+    *darwin*)
+        echo "MacOS Detected";
+      ;;
+    *) 
+        echo "Cannot Determine OS";
+      ;;
+  esac
+
+  echo $os;
+}
+
+
+detect_os
+OS=$
+
+echo $OS
